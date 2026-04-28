b0f849eab111a5e9f80d18e2d217083d46e9df5b
JustTalhaRehman <talha32rehman@gmail.com>
2026-04-28 23:02:19 +0500

    Fix Terraform module source to use specific version tag

--- terraform/pod-identity.tf
+++ terraform/pod-identity.tf
@@ -2,7 +2,7 @@
 # This creates an IAM role that can be assumed by the External Secrets Operator
 # service account to access AWS Secrets Manager
 module "external_secrets_irsa" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks?ref=v5.39.0"
   version = "~> 5.0"
 
   # Role name based on cluster name
